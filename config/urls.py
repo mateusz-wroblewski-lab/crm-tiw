becca897d87984f42a121138415617bbaeed75e7
mateusz-wroblewski-lab <155849345+mateusz-wroblewski-lab@users.noreply.github.com>
2024-08-06 22:29:13 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('secret-panel-admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('crm.urls')),
     path('management/', include('management.urls')),
     path('members/', include('members.urls')),
